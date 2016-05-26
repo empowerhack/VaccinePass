31b3228fab5b07b0e4371818efd95f64b2a16b33
Fahad Iqbal <iqbal.fahad90@gmail.com>
2016-05-26 14:54:35 +0100

    Init

---
+++ App/VPApp/platforms/android/assets/www/js/services.js
@@ -0,0 +1,78 @@
+angular.module('vpApp.services', [])
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Dover',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+.factory('VaccinePassStorage', function ($localStorage) {
+    $localStorage = $localStorage.$default({
+        things: [{
+            id: 0,
+            name: 'Ben Dover',
+            lastText: 'You on your way?'
+        }]
+    });
+
+    var _getAll = function () {
+        return $localStorage.things;
+    };
+
+    var _add = function (thing) {
+        $localStorage.things.push(thing);
+    }
+
+    var _remove = function (thing) {
+        $localStorage.things.splice($localStorage.things.indexOf(thing), 1);
+    }
+
+    return {
+        getAll: _getAll,
+        add: _add,
+        remove: _remove
+    };
+});
---
+++ App/VPApp/www/js/controllers.js
@@ -0,0 +1,38 @@
+angular.module('vpApp.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('PassesCtrl', function ($scope, VaccinePassStorage) {
+    $scope.things = VaccinePassStorage.getAll();
+    $scope.add = function (newThing) {
+        VaccinePassStorage.add(newThing);
+    };
+    $scope.remove = function (thing) {
+        VaccinePassStorage.remove(thing);
+    };
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
